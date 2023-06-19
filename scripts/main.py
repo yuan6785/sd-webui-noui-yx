bfe8c6cbdd940087033401525129a88e7f8b49ac
mac yx <fsdf@f.com>
2023-06-19 16:31:43 +0800

    1

--- scripts/main.py
+++ scripts/main.py
@@ -11,7 +11,7 @@ import requests
 from gradio import Blocks
 from fastapi import FastAPI
 from typing import Optional, Dict, Any
-
+import socket
 try:
     from modules.paths import extensions_dir, script_path
 
@@ -30,6 +30,23 @@ except ImportError:
 EXT_INNER_PATH = Path(scripts.basedir())
 
 
+# def on_ui_settings():
+#     """
+#     @des: 启动时候动态设置配置项
+#     参考:
+#     /Users/yuanxiao/workspace/0yxgithub/stable-diffusion-webui/modules/shared.py
+#     参考
+#     /Users/yuanxiao/workspace/0yxgithub/stable-diffusion-webui/modules/script_callbacks.py
+#     """
+#     if 1:
+#         TAC_SECTION = ("playdayy", "天天玩家设置")
+#         # 添加自定义配置项
+#         shared.opts.add_option("playdayyconfig", shared.OptionInfo(default="", label="天天玩家额外配置", section=TAC_SECTION))
+#         # 启动时候动态设置配置项---这里是有效果的------
+#         shared.opts.set("playdayyconfig", "yxtest")   # 自定义配置选项----云函数可以配合启动命令将域名动态传入然后创建一个文件，然后启动sd的时候读取这个文件，然后设置这个配置项（其实云函数拿到url域名更方便的是用带有界面的扩展，直接用js读取传到后端即可--重要）； ecs不用这个
+#         shared.opts.set("outdir_samples", "yxtest6666")   # 系统配置选项
+#         # print("动态设置输出路径配置项完成-------by yx")
+
 def on_ui_settings():
     """
     @des: 启动时候动态设置配置项
@@ -38,14 +55,14 @@ def on_ui_settings():
     参考
     /Users/yuanxiao/workspace/0yxgithub/stable-diffusion-webui/modules/script_callbacks.py
     """
-    if 1:
-        TAC_SECTION = ("playdayy", "天天玩家设置")
-        # 添加自定义配置项
-        shared.opts.add_option("playdayyconfig", shared.OptionInfo(default="", label="天天玩家额外配置", section=TAC_SECTION))
-        # 启动时候动态设置配置项---这里是有效果的------
-        shared.opts.set("playdayyconfig", "yxtest")   # 自定义配置选项
-        shared.opts.set("outdir_samples", "yxtest6666")   # 系统配置选项
-        # print("动态设置输出路径配置项完成-------by yx")
+    # 获取当前ubuntu服务器的主机名
+    hostname = socket.gethostname()
+    try:
+        username = hostname.split("-")[-1].strip()
+    except:
+        username = "unknown"
+    shared.opts.set("outdir_samples", f"outputs/{username}")   # 系统配置选项
+    # print("动态设置输出路径配置项完成-------by yx")
         
 
 def on_app_started(demo: Optional[Blocks], app: FastAPI):
